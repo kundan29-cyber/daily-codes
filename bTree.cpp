6ba95b8e51b043f558d540f04e88f8dddc44878d
Kundan Krishnan <72137809+kundan29-cyber@users.noreply.github.com>
2022-10-02 12:39:17 +0530

    Add files via upload

---
+++ bTree.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std
+#define ll long long int
+
+class node
+{
+public:
+    int data;
+    node* left;
+    node* right;
+
+    node(int val)
+    {
+        data=val;
+        left=right=NULL:
+    }
+};
+
+int main()
+{
+    node root=new node(1);
+    root->left=new node(2);
+    root->right=new node(3);
+    root->left->right=new node(4);
+}
